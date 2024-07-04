5bb8eea25b5c13dc9a280f833da882ce4fad6e59
David <deeseinmedia@gmail.com>
2024-07-04 12:15:47 +0200

    added public route tables ans associate them

--- vpc.tf
+++ vpc.tf
@@ -30,3 +30,24 @@ resource "aws_subnet" "public" {
   }
 
 }
+
+# Create Route Tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.cidr_blocks[0]
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public_Route_Table ${var.tagNameDate}"
+  }
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
