74a0ad2e69beb20010abe750c7088392bcb212ce
David <deeseinmedia@gmail.com>
2024-07-04 15:03:09 +0200

    adding variables for EC2 and RDS

--- variables.tf
+++ variables.tf
@@ -22,3 +22,27 @@ variable "private_subnet_cidr_blocks" {
   description = "CIDR blocks for private subnets"
   default     = ["10.0.0.128/26", "10.0.0.192/26"] # Adjust as needed
 }
+
+# EC2 Variables
+variable "ec2_instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "vockey"
+}
+
+# Variables for RDS DB Instance
+variable "db_username" {
+  description = "Username for the DB instance"
+  default = "root"
+}
+variable "db_password" {
+  description = "Password for the DB instance"
+  default = "password123"
+  sensitive = true
+}
+variable "rds_db_name" {
+  description = "Name of the RDS DB instance"
+  default = "myDatabase"
+}
